7c7db7098e5da16550696a403551846ecc5c11f4
Ibraheem <bolex@MSI>
2026-03-27 19:25:56 -0400

    Created song structure,queue structure , playlist structure

--- TBSD_Group1_Playlist/TBSD_Group1_Playlist/musicplayer.h
+++ TBSD_Group1_Playlist/TBSD_Group1_Playlist/musicplayer.h
@@ -3,3 +3,25 @@
 #define MUSIC_PLAYER_H
 
 #define MAX_LENGTH 100
+
+/* Song structure */
+typedef struct {
+    char artistName[MAX_LENGTH];
+    char songName[MAX_LENGTH];
+    int timesPlayed;
+} Song;
+
+/* Queue structure */
+typedef struct {
+    Song songs[50];
+    int front;
+    int rear;
+} Queue;
+
+/* Playlist structure */
+typedef struct {
+    Song songs[100];
+    int count;
+} Playlist;
+
+#endif

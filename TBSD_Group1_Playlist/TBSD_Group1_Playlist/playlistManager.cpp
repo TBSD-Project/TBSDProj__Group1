276d9906f66925fff2c7cc22b808889395d702dd
Ibraheem <bolex@MSI>
2026-04-09 16:18:44 -0400

    wrote the playlistManager in c++

--- TBSD_Group1_Playlist/TBSD_Group1_Playlist/playlistManager.cpp
+++ TBSD_Group1_Playlist/TBSD_Group1_Playlist/playlistManager.cpp
@@ -1,57 +1,44 @@
-#include <stdio.h>
-#include <string.h>
 #include "playlistManager.h"
+#include <algorithm>
 
-void createPlaylist(Playlist* p, char name[]) {
-    strcpy(p->playlistName, name);
-    p->songCount = 0;
+void PlaylistManager::removePlaylist(Playlist& playlist) {
+    playlist.songs.clear();
 }
 
-void addSongToPlaylist(Playlist* p, char artist[], char song[]) {
+void PlaylistManager::addSong(Playlist& playlist, const Song& song) {
+    playlist.songs.push_back(song);
+}
 
-    if (p->songCount >= MAX_SONGS) {
-        printf("Playlist is full\n");
-        return;
+void PlaylistManager::removeSong(Playlist& playlist, int index) {
+
+    if (index >= 0 && index < playlist.songs.size()) {
+        playlist.songs.erase(playlist.songs.begin() + index);
     }
 
-    strcpy(p->songs[p->songCount].artistName, artist);
-    strcpy(p->songs[p->songCount].songName, song);
-    p->songs[p->songCount].timesPlayed = 0;
-
-    p->songCount++;
 }
 
-void removeSongFromPlaylist(Playlist* p, int index) {
+void PlaylistManager::reorderSong(Playlist& playlist, int oldIndex, int newIndex) {
 
-    if (index < 0 || index >= p->songCount) {
-        printf("Invalid song index\n");
-        return;
+    if (oldIndex >= 0 && oldIndex < playlist.songs.size() &&
+        newIndex >= 0 && newIndex < playlist.songs.size()) {
+
+        Song temp = playlist.songs[oldIndex];
+
+        playlist.songs.erase(playlist.songs.begin() + oldIndex);
+        playlist.songs.insert(playlist.songs.begin() + newIndex, temp);
     }
 
-    for (int i = index; i < p->songCount - 1; i++) {
-        p->songs[i] = p->songs[i + 1];
-    }
-
-    p->songCount--;
 }
 
-void deletePlaylist(Playlist* p) {
-    p->songCount = 0;
+void PlaylistManager::renamePlaylist(Playlist& playlist, string newName) {
+    playlist.name = newName;
 }
 
-void renamePlaylist(Playlist* p, char newName[]) {
-    strcpy(p->playlistName, newName);
-}
+void PlaylistManager::sortPlaylistByName(Playlist& playlist) {
 
-void displayPlaylist(Playlist* p) {
+    sort(playlist.songs.begin(), playlist.songs.end(),
+        [](Song a, Song b) {
+            return a.name < b.name;
+        });
 
-    printf("Playlist: %s\n", p->playlistName);
-
-    for (int i = 0; i < p->songCount; i++) {
-        printf("%d. %s - %s (Played %d times)\n",
-            i + 1,
-            p->songs[i].artistName,
-            p->songs[i].songName,
-            p->songs[i].timesPlayed);
-    }
 }

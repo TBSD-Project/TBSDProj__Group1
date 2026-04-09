1a12e75352de52ef66e6b9abbd9cb218c1af61a3
Ibraheem <bolex@MSI>
2026-04-09 16:22:07 -0400

    updated the main.c

--- TBSD_Group1_Playlist/TBSD_Group1_Playlist/main.cpp
+++ TBSD_Group1_Playlist/TBSD_Group1_Playlist/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "playlistManager.h"
+#include "songs.h"
+
 using namespace std;
 
 /*
@@ -8,10 +11,29 @@ CHANGED AND REVERTED CONFLICTING FILES
 */
 
 
-//int main() {
-//
-//
-//	cout << "Testing testing";
-//
-//	return 0;
-//}
+int main() {
+
+    PlaylistManager manager;
+
+    PlaylistManager::Playlist playlist;
+    playlist.name = "My Playlist";
+
+    Song song1;
+    song1.name = "Blinding Lights";
+
+    Song song2;
+    song2.name = "God's Plan";
+
+    manager.addSong(playlist, song1);
+    manager.addSong(playlist, song2);
+
+    manager.renamePlaylist(playlist, "Favorite Songs");
+
+    cout << "Playlist: " << playlist.name << endl;
+
+    for (int i = 0; i < playlist.songs.size(); i++) {
+        cout << playlist.songs[i].name << endl;
+    }
+
+    return 0;
+}

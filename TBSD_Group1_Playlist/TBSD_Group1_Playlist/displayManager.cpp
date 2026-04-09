acc4e2a361cc8e48036f1547493393b9858ce5ee
Ibraheem <bolex@MSI>
2026-04-09 16:30:08 -0400

    Worked on the diplayManager.c

--- TBSD_Group1_Playlist/TBSD_Group1_Playlist/displayManager.cpp
+++ TBSD_Group1_Playlist/TBSD_Group1_Playlist/displayManager.cpp
@@ -1,23 +1,29 @@
-
 #include "displayManager.h"
+#include <iostream>
+using namespace std;
 
-
-
-
-
-void displayArtist(const Song& song) {
-
-
-}
-void displayAlbum(const Song& song) {
-
-
-}
-void displayTimesPlayed(const Song& song) {
-
+void DisplayManager::displayArtist(const Song& song) {
+    if (song.getArtist().empty()) {
+        cout << "Artist: Unknown Artist" << endl;
+    }
+    else {
+        cout << "Artist: " << song.getArtist() << endl;
+    }
 }
 
+void DisplayManager::displayAlbum(const Song& song) {
+    if (song.getAlbum().empty()) {
+        cout << "Album: Unknown Album" << endl;
+    }
+    else {
+        cout << "Album: " << song.getAlbum() << endl;
+    }
+}
 
-void displayTitle(const Song& song) {
+void DisplayManager::displayTimesPlayed(const Song& song) {
+    cout << "Times Played: " << song.getPlaycount() << endl;
+}
 
+void DisplayManager::displayTitle(const Song& song) {
+    cout << "Title: " << song.getTitle() << endl;
 }
